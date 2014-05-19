33579d43af021df13493b9945037b8ee9aebb68a
Homme Zwaagstra <hrz@geodata.soton.ac.uk>
2014-05-19 12:18:51 +0100

    Implement the majority of plugin features

--- plugin/quickdraw.py
+++ plugin/quickdraw.py
@@ -23,12 +23,26 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from qgis.core import *
+from qgis.gui import *
 # Initialize Qt resources from file resources.py
 import resources_rc
 # Import the code for the dialog
 from quickdrawdialog import QuickDrawDialog
 import os.path
 
+class QuickDrawError(Exception):
+    def __init__(self, message, title):
+        super(QuickDrawError, self).__init__(message)
+        self.title = title
+
+class InvalidGeometry(QuickDrawError):
+    def __init__(self, geom):
+        if len(geom) > 50:
+            msg = geom[:24].rstrip() + ' ... ' + geom[-24:].lstrip()
+        else:
+            msg = geom
+        super(InvalidGeometry, self).__init__("The geometry formatting is wrong: %s" % msg, 'Invalid Geometry')
+        self.geom = geom
 
 class QuickDraw:
 
@@ -51,6 +65,9 @@ class QuickDraw:
         # Create the dialog (after translation) and keep reference
         self.dlg = QuickDrawDialog()
 
+        # the draw stack contains references to all items drawn by the plugin
+        self.drawStack = []
+
     def initGui(self):
         # Create action that will start plugin configuration
         self.action = QAction(
@@ -63,19 +80,151 @@ class QuickDraw:
         self.iface.addToolBarIcon(self.action)
         self.iface.addPluginToMenu(u"&Quick Draw", self.action)
 
+        QObject.connect(self.dlg.clearButton, SIGNAL("clicked()"), self.clearButtonClicked)
+
+    def clearButtonClicked(self):
+        self.dlg.geometryTextEdit.setPlainText('')
+
     def unload(self):
         # Remove the plugin menu item and icon
         self.iface.removePluginMenu(u"&Quick Draw", self.action)
         self.iface.removeToolBarIcon(self.action)
+        self.removeItems()
 
     # run method that performs all the real work
     def run(self):
+        # save the text so that it can be reverted in case of cancel
+        text = self.dlg.geometryTextEdit.toPlainText()
+
         # show the dialog
         self.dlg.show()
         # Run the dialog event loop
         result = self.dlg.exec_()
         # See if OK was pressed
         if result == 1:
-            # do something useful (delete the line containing pass and
-            # substitute with your code)
-            pass
+            text = str(self.dlg.geometryTextEdit.toPlainText())
+
+            drawStack = []
+            for line in [l.strip() for l in text.splitlines()]:
+                if not line: continue
+
+                try:
+                    geom = self.textToGeometry(line)
+                    r = self.geometryToCanvas(geom)
+                    r.show()
+                    drawStack.append(r)
+                except QuickDrawError, e:
+                    QMessageBox.warning(self.dlg, e.title, e.message)
+                    self.removeItems(drawStack) # remove added items
+                    return self.run() # try again
+
+            self.removeItems()  # remove previous items
+            self.drawStack = drawStack
+
+            if self.dlg.zoomCheckBox.isChecked():
+                self.zoomToItems()
+        else:
+            # cancel was pressed: ensure the text doesn't change
+            self.dlg.geometryTextEdit.setPlainText(text)
+
+    def zoomToItems(self):
+        def getBBOX(item):
+            if isinstance(item, QgsVertexMarker):
+                return QgsRectangle(item.point, item.point)
+            return item.asGeometry().boundingBox()
+        
+        if not self.drawStack:
+            return
+
+        canvas = self.iface.mapCanvas()
+        extent = getBBOX(self.drawStack[0])
+        for item in self.drawStack[1:]:
+            bbox = getBBOX(item)
+            extent.combineExtentWith(bbox)
+
+        if extent:
+            canvas.setExtent(extent)
+            canvas.updateFullExtent()
+
+    def removeItems(self, drawStack = None):
+        if drawStack is None:
+            drawStack = self.drawStack
+
+        # Remove all items from the canvas
+        scene = self.iface.mapCanvas().scene()
+        while drawStack:
+            scene.removeItem(drawStack.pop())
+
+    def textToCoords(self, text):
+        def toCoords(text):
+            try:
+                coords = [float(t.strip()) for t in text.split(',')]
+            except ValueError:
+                raise InvalidGeometry(text)
+
+            if not len(coords) % 2: # we have an even number of coordinates
+                x = None
+                points = []
+                for i, coord in enumerate(coords):
+                    if i % 2:
+                        points.append((x, coord))
+                    else:
+                        x = coord
+                return points
+
+            raise QuickDrawError('An even number of coordinates is expected but %d were input.' % len(coords), 'Invalid Coordinate Count')
+
+        if ':' in text:
+            # it's a bbox
+            try:
+                minc, maxc = text.split(':')
+                minx, miny = toCoords(minc)[0]
+                maxx, maxy = toCoords(maxc)[0]
+            except ValueError:
+                raise InvalidGeometry(text)
+                
+            return [(minx, miny), (maxx, miny), (maxx, maxy), (minx, maxy), (minx, miny)]
+        
+        # it's a normal coordinate string
+        return toCoords(text)
+
+    def coordsToPoints(self, coords):
+        return [QgsPoint(*coord) for coord in coords]
+
+    def textToGeometry(self, text):
+        coords = self.textToCoords(text)
+        coord_count = len(coords)
+        points = self.coordsToPoints(coords)
+
+        if coord_count == 1:
+            return points[0]
+        elif coord_count > 2 and coords[0] == coords[-1]:
+            # it's a polygon
+            return QgsGeometry.fromPolygon([points])
+        
+        return QgsGeometry.fromPolyline(points)
+
+    def geometryToCanvas(self, geom):
+        if isinstance(geom, QgsPoint):
+            r = self.pointToVertexMarker(geom)
+        else:
+            r = self.geometryToRubberBand(geom)
+        
+        r.setColor(QColor(255,0,0))
+        r.setOpacity(0.5)
+        return r
+
+    def pointToVertexMarker(self, point):
+        canvas = self.iface.mapCanvas()
+        m = QgsVertexMarker(canvas)
+        m.setCenter(point)
+        m.setPenWidth(2.5)
+        m.point = point         # save the original point for use in zooming
+        return m
+        
+    def geometryToRubberBand(self, geom):
+        canvas = self.iface.mapCanvas()
+        r = QgsRubberBand(canvas, False)
+        r.setToGeometry(geom, None)
+        r.setWidth(2.5)
+        return r
